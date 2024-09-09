9ea8fc0d913f5aeaa68050e0f2e640cb7b45b5de
catalunha <catalunha.mj@gmail.com>
2024-09-09 09:54:24 -0300

    rev

---
+++ dunder/__all__/em-arquivo/code_1.py
@@ -0,0 +1,9 @@
+waz = 5
+bar = 10
+
+
+def baz():
+    return "baz"
+
+
+__all__ = ["bar", "baz"]
---
+++ dunder/__all__/em-arquivo/code_2.py
@@ -0,0 +1,8 @@
+from code_1 import *
+
+print(bar)
+print(baz)
+
+# The following will trigger an exception, as "waz" is not exported by the module
+# A linha seguinte dispara uma exception. "waz" não é exportado como môdulo
+# print(waz) # error
